88eca1ef00f514023d826c7ba2f8be49aae48474
Jason <jason.walls301@gmail.com>
2024-02-28 02:36:26 +1100

    included bigw

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-const elements = document.querySelectorAll('p');
+let timer = 0;
+let bigw_interval;
 
-elements.forEach(element => {
-    if (element.textContent.toLowerCase().includes("traditional custodians") || element.textContent.toLowerCase().includes("traditional owners")) {
-        if (element.parentElement != null){
-            element.parentElement.style.visibility = "hidden";
-        } else {
-            element.style.visibility = "hidden";
+if (window.location.href.includes("bigw.com.au")){
+    bigw_interval = setInterval(remove_ack_bigw, 10);
+}
+
+window.addEventListener("load", remove_ack_paragraphs);
+
+// remove acknowledgements from dynamic websites that aren't covered by the general cases
+function remove_ack_bigw() {
+        let element = document.querySelector('.footer-section.footer-country');
+        if (element) {
+            element.remove();
         }
-    }
-});
+        timer += 10;
+        if (timer >= 500){
+            clearInterval(bigw_interval);
+        }
+}
+
+// remove acknowledgements in general cases
+function remove_ack_paragraphs() {
+    const elements = document.querySelectorAll('p');
+
+    elements.forEach(element => {
+        if (element.textContent.toLowerCase().includes("traditional custodians") || element.textContent.toLowerCase().includes("traditional owners")) {
+            if (element.parentElement != null) {
+                element.parentElement.style.visibility = "hidden";
+            } else {
+                element.style.visibility = "hidden";
+            }
+        }
+    });
+}
