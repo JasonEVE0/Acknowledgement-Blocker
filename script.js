fe9af6d8a6991d6e2f82869dad04fb418543fc62
Jason <jason.walls301@gmail.com>
2024-02-28 00:52:56 +1100

    updated list of terminology

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const elements = document.querySelectorAll('p');
 
 elements.forEach(element => {
-    if (element.textContent.toLowerCase().includes("traditional custodians")) {
+    if (element.textContent.toLowerCase().includes("traditional custodians") || element.textContent.toLowerCase().includes("traditional owners")) {
         if (element.parentElement != null){
             element.parentElement.style.visibility = "hidden";
         } else {
